92810dbe0ad29418964f45cd8c4e4b121dbf9e50
adrianWarrior <adrianoforcellinipedretti@gmail.com>
2021-10-25 00:19:53 -0300

    iniciando front

---
+++ Impulse/NLW_REACT/src/App.tsx
@@ -0,0 +1,12 @@
+
+import styles from './App.module.css'
+function App() {
+
+  return (
+    <main className={styles.contentWrapper}>
+      <h1>  Hello Vite + React!</h1>
+    </main>
+  )
+}
+
+export default App
